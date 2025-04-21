78e3a9620d81ff8140881647c3fb1557ebde5820
ryankusuma <ryankusuma.atwork@gmail.com>
2025-04-21 09:20:28 +0700

    ryan : fix type phone input

--- end-user/src/components/partials/Field/FieldPhoneInput.tsx
+++ end-user/src/components/partials/Field/FieldPhoneInput.tsx
@@ -7,11 +7,10 @@ import { Control } from "react-hook-form";
 type Props = {
     control: Control<any>,
     name: string,
-    label: string,
-    type?: "textarea" | ""
+    label: string
 }
 
-export default function FieldPhoneInput({ control, name, label, type }: Props) {
+export default function FieldPhoneInput({ control, name, label }: Props) {
     return (
         <FormField
             name={name}
@@ -40,22 +39,3 @@ export default function FieldPhoneInput({ control, name, label, type }: Props) {
             )} />
     )
 }
-// <div className="flex flex-col gap-1 w-full">
-//     <label htmlFor="phone" className="text-white">Phone Number <span>*</span></label>
-//     <PhoneInput
-//         defaultCountry='sg'
-//         value={getValues("phone")}
-//         onChange={(value) => setValue("phone", value, { shouldValidate: true })}
-//         inputClassName="!flex !items-center !text-base !h-12 !rounded-e-md w-full bg-blue-900"
-//         style={
-//             {
-//                 '--react-international-phone-flag-width': '24px',
-//                 '--react-international-phone-flag-height': '24px',
-//                 '--react-international-phone-height': '48px',
-//             } as React.CSSProperties
-//         }
-//     />
-
-// </div>
-// )
-// }
