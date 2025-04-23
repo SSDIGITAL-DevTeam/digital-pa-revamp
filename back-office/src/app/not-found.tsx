7f205956e58fd65e8c6c0790e6237ff93a3d459a
ryankusuma <ryankusuma.atwork@gmail.com>
2025-04-23 13:37:53 +0700

    ryan : add 404 page

--- back-office/src/app/not-found.tsx
+++ back-office/src/app/not-found.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+"use client"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import NotFoundGif from '@/asset/not-found/404 dpa.gif'
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-const NotFound = () => {
+export default function NotFound() {
+  const router = useRouter()
+
+
   return (
-    <div>Halaman Tidak Ditemukan</div>
+    <main className="w-full h-screen bg-white lg:pt-44">
+      <section className="max-w-7xl mx-auto h-full flex flex-col justify-center lg:justify-start items-center lg:items-start gap-2 lg:gap-6 relative px-10">
+        <div className="lg:hidden flex flex-col items-center font-extrabold text-primary gap-5 mb-6">
+          <p className="text-2xl lg:text-5xl">Oops! Error</p>
+          <Image src={NotFoundGif} alt="404" width={400} height={400} className="w-full sm:max-w-[500px]" />
+        </div>
+        <h1 className="text-center md:text-left text-xl md:text-3xl lg:text-5xl font-semibold">Sorry, This Page Doesn’t Exist</h1>
+        <p className="text-center md:text-left text-sm md:text-xl !leading-[130%]">the page you’re looking for doesn’t exist or may have been moved.</p>
+        <Button onClick={() => router.back()} className="py-6 px-8 font-semibold rounded-xl lg:mt-0 mt-7 bg-red-700">Return to the Homepage</Button>
+        <div className="hidden absolute bottom-10 right-10 lg:flex flex-col items-end font-bold text-red-700 gap-8">
+          <p className="text-5xl">Oops! Error</p>
+          <Image src={NotFoundGif} alt="404" width={400} height={400} className="w-full" />
+        </div>
+      </section>
+    </main>
   )
 }
-
-export default NotFound
