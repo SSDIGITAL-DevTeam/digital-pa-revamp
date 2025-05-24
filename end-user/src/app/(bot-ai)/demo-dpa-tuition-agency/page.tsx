5014629aca5b1fc8d7e55669965a8ddcaac097ab
ryankusuma <ryankusuma.atwork@gmail.com>
2025-05-24 20:41:53 +0700

    ryan : update text demo chatbot

--- end-user/src/app/(bot-ai)/demo-dpa-tuition-agency/page.tsx
+++ end-user/src/app/(bot-ai)/demo-dpa-tuition-agency/page.tsx
@@ -9,7 +9,7 @@ export default function ChatBot() {
       <Script
         src="https://widgets.leadconnectorhq.com/loader.js"
         data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js"
-        data-widget-id="67d6a0305f21d1140f10ee9d"
+        data-widget-id="68319911619d7c547d1806bc"
         strategy="afterInteractive"
       />
       <div className="flex flex-col gap-4 items-center justify-center z-[10]">
@@ -20,8 +20,8 @@ export default function ChatBot() {
           height={400}
           className="w-[40vw] max-w-[230px] object-contain wiggle"
         />
-        <h1 className="text-2xl sm:text-3xl md:text-5xl text-center !leading-snug">Digital PA <br />
-        <span className="text-primary">AI Chatbot Testing</span></h1>
+        <h1 className="text-2xl sm:text-3xl md:text-5xl text-center !leading-snug">DPA Tuition Centre<br />
+        <span className="text-primary">AI Chatbot Demo</span></h1>
       </div>
       <div>
         <Image src={GradientRightSVG} alt="" width={1920} height={1080} className="absolute top-0 -right-[50%] scale-[200%] lg:top-[-100vh] lg:-right-[56vw] opacity-80 -rotate-90" />
