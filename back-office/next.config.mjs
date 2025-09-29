b6019f240abbe3ec4f9ae9317b75dc1e9b1d4f24
heriyanto <heriyanto.atwork@gmail.com>
2025-09-29 11:29:35 +0700

    update next config

--- back-office/next.config.mjs
+++ back-office/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "api.digital-pa.com.sg"],
+    domains: ["localhost", "api.digital-pa.com.sg","dev-api.digital-pa.com.sg"],
   },
   eslint: {
     ignoreDuringBuilds: true, // Matikan ESLint saat build
